a0e4d60bb832f89d81468ba2e8e10f51fd230181
AlinaMaris <alina.marisova@jetbrains.com>
2025-05-26 15:55:54 +0200

    Add files via upload

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import TravelPlanner from './App'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <TravelPlanner />
+  </React.StrictMode>,
+)
